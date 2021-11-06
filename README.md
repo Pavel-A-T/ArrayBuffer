bb299a4580b24fc48dea00c3fdc62822583aaf68
Pavel Titov <otrepiev.grishka@yandex.ru>
2021-11-07 00:52:00 +0300

    ArrayBuffer homeWork

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ArrayBuffer
-ArrayBuffer homework netology
+ArrayBuffer homework netology [![Build status](https://ci.appveyor.com/api/projects/status/le7bprwrqybe5gft/branch/master?svg=true)](https://ci.appveyor.com/project/Pavel-A-T/arraybuffer/branch/master)
+
---
+++ src/arrayBufferConverter.test.js
@@ -0,0 +1,9 @@
+import getBuffer from './index';
+import ArrayBufferConverter from './arrayBufferConverter';
+
+test(('arrayBufferConverter toString'), () => {
+  const expected = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  const arrayBufferConverter = new ArrayBufferConverter();
+  arrayBufferConverter.load(getBuffer());
+  expect(arrayBufferConverter.toString()).toBe(expected);
+});
